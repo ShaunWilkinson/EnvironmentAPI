d567c0a83f29a68a4afbc0fef81d5cd119a264d6
Shaun <shaundavidwilkinson@gmail.com>
2021-01-31 13:28:41 +0000

    Added data processor

---
+++ processIotHubData/index.js
@@ -0,0 +1,29 @@
+module.exports = function (context, IoTHubMessages) {
+    context.log(`JavaScript eventhub trigger function called for message array: ${IoTHubMessages}`);
+
+    var count = 0;
+    var temperature = 0.0;
+    var humidity = 0.0;
+    var deviceId = "";
+    
+    IoTHubMessages.forEach(message => {
+        context.log(`Processed message: ${message}`);
+        count++;
+        temperature += message.temperature;
+        humidity += message.humidity;
+        deviceId = message.deviceId;
+    });
+
+    var output = {
+        "deviceId": deviceId,
+        "measurementsCount": count,
+        "temperature": temperature,
+        "humidity": humidity
+    }
+
+    context.log(`output content: ${output}`);
+
+    context.cosmosDBOutput.outputDocument = output;
+
+    context.done();
+};
